d11fcd8c7a3da1c1f18652c94b1e68a5f1600ea8
Matouš Šimůnek <1165@student.itgymnazium.cz>
2023-11-09 11:44:46 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Security;
+
+Console.WriteLine("Hello, World!");
+
+string text;// = "Hello, World!";
+int a = 10;
+float x = 4.654432132f;
+double b = 6.123;
+
+bool z = false;
+char c = 'a';
+
+double result = a + x + b;
+
+int zero = 0;
+double bz = 4.2 / 5;
+
+int znamka = 5
+    ;
+bool foo = 2 < 50;
+
+int input = 2;
+if (znamka > 6)
+{
+    Console.WriteLine(zero);
+}
+if (input == 3)
+{
+    Console.WriteLine("oh my heavens! It is mr. Buckleworth");
+}
+else
+{
+    Console.WriteLine("oh no~! That is not good, mr. Buckleworth will be late!");
+}
